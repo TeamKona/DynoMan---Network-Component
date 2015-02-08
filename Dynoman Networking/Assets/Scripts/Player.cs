8ca072e1df39e5606d93ac843df3e77ae3d50c97
AJBucur <alexandrebucur@gmail.com>
2015-02-08 18:04:25 -0500

    Basic Multiplayer Component
    Underlaying the basic multiplayer component based on Unity Networking
    reasearch

---
+++ Dynoman Networking/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (networkView.isMine){
+			if (Input.GetKey(KeyCode.W))
+				rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+			
+			if (Input.GetKey(KeyCode.S))
+				rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+			
+			if (Input.GetKey(KeyCode.D))
+				rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+			
+			if (Input.GetKey(KeyCode.A))
+				rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+		}
+		else {
+			enabled = false;
+		}
+	}
+}
